43035dc31887a48718b418d4708859dff8947572
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2020-08-11 16:28:17 +0300

    Began input-validator.py

---
+++ matcher/input-validator.py
@@ -0,0 +1,16 @@
+'''
+Created on 11 Aug 2020
+
+@author: atilante
+
+Input validator for studying input requirements of the Build-heap exercise.
+'''
+
+# TODOs:
+# - create algorithmic definitions of main loop variants in buildheap.py
+# - use dok/aalto/jatko-opinnot/VAS-misconceptions/preconfigured_templates/template1.dia
+heap_input = [11, 33, 11, 28, 12, 50, 26, 15, 15, 12, 40, 30, 48, 12, 70,
+              10, 6, 16, 18, 60, 14, 35, 35, 25, 28, 44, 35, 14]
+
+if __name__ == '__main__':
+    pass
